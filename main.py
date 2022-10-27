0e056000355bb9c6534232dde280e68d77dade21
DebuggedMoon <predojevic@proton.me>
2022-10-27 09:15:37 +0100

    add create_layout function

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 """A small typing game named "Typing Skirmish" using a TUI (Text User Interface)."""
 
+from rich.layout import Layout
+
 from textual.app import App
 from textual.widgets import Placeholder
+
+def create_layout() -> Layout:
+    """Define and create layout of application."""
+    
+    root = Layout(name="root")
+    root.split_row(
+        Layout(name="info-bar", size=6),
+        Layout(name="main"),
+        Layout(name="footer", size=5)
+    )
+
+    root["info-bar"].split_column(
+        Layout(name="player"),
+        Layout(name="middle"),
+        Layout(name="opponent")
+    )
+
+    return root
 
 class Game(App):
     """Class for handling game state and Textual App / TUI rendering."""
@@ -9,6 +29,10 @@ class Game(App):
     async def on_mount(self) -> None:
         """Handle and Render Layouts."""
         await self.view.dock(Placeholder())
+
+    async def on_key(self, event) -> None:
+        """Handle Key Proccessing here."""
+        print(event.key)
 
 def main() -> None:
     """Initilize main logic and "Typing Skirmish" app."""
@@ -19,4 +43,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-    
